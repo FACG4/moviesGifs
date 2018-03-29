e77d0c7b4f73a92b638dbfc61a88d37e70f92451
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-03-29 09:06:36 +0200

    solve problem

---
+++ Test/test.js
@@ -0,0 +1,22 @@
+var test = require('tapes');
+
+
+
+
+test('Example test', function(t) {
+  t.pass();
+  t.end();
+});
+// test('movieSearch', function(t) {
+//   const acual =
+//   const exp =
+//   t.deepEqual(acual, exp, 'return result');
+//   t.end();
+//
+// });
+test("movieSearch",function(t) {
+  var actual=movieSearch();
+  var expected=;
+  t.equal(actual, expected, "back movie");
+
+})
